9cbc4c78ce83d61846940d570aa406730b632680
John Patterson <john@johnppatterson.com>
2021-03-27 18:29:08 -0700

    added navbar

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,14 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <body className="App-header">
+      <head className="App-header">
+        <div className="App-header-bar">
+          <div className="App-header-bar-item">About</div>
+          <div className="App-header-bar-item">Resume</div>
+          <div className="App-header-bar-item">Works</div>
+        </div>
+      </head>
+      <body className="App-body">
         <p>This is a test of my CSS <a href="google.com">Link to Google</a>.</p>
       </body>
     </div>
