35b15f28eb6bc56e5ca30d231e4455d23ada34f8
John Patterson <john@johnppatterson.com>
2021-03-27 19:02:44 -0700

    added routing for different pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,77 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Link,
+  Route
+} from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <head className="App-header">
-        <div className="App-header-bar">
-          <div className="App-header-bar-item">About</div>
-          <div className="App-header-bar-item">Resume</div>
-          <div className="App-header-bar-item">Works</div>
-        </div>
-      </head>
-      <body className="App-body">
-        <p>This is a test of my CSS <a href="google.com">Link to Google</a>.</p>
-      </body>
+      <Router>
+        <head className="App-header">
+          <div className="App-header-bar">
+            <Link className="App-header-bar-item" to="/">Home</Link>
+            <Link className="App-header-bar-item" to="/about">About</Link>
+            <Link className="App-header-bar-item" to="/resume">Resume</Link>
+            <Link className="App-header-bar-item" to="/works">Works</Link>
+          </div>
+        </head>
+        <body className="App-body">
+          <Switch>
+            <Route path="/" exact>
+              <HomePage />
+            </Route>
+            <Route path="/about">
+              <AboutPage />
+            </Route>
+            <Route path="/resume">
+              <ResumePage />
+            </Route>
+            <Route path="/works">
+              <WorksPage />
+            </Route>
+          </Switch>
+        </body>
+      </Router>
     </div>
   );
 }
+
+function HomePage(): JSX.Element {
+  return (
+    <>
+      <p>Home page</p>
+    </>
+  );
+}
+
+function AboutPage(): JSX.Element {
+  return (
+    <>
+      <p>About page</p>
+    </>
+  );
+}
+
+function ResumePage(): JSX.Element {
+  return (
+    <>
+      <p>Resume page</p>
+    </>
+  );
+}
+
+function WorksPage(): JSX.Element {
+  return (
+    <>
+      <p>Works page</p>
+    </>
+  );
+}
+
+
 
 export default App;
