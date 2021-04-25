760ef9f54a6126c494607e7554457541b969ea41
John Patterson <john@johnppatterson.com>
2021-04-25 00:11:31 -0700

    piped data in resume page

--- src/data/index.ts
+++ src/data/index.ts
@@ -2,12 +2,3 @@ export { educationHistory as education } from "./education";
 export { jobs } from "./jobs";
 export { projects } from "./projects";
 export { skills } from "./skills";
-
-export type {
-    Job,
-    Project,
-    Education,
-    SkillProficiency,
-    SkillCategory,
-    Skill
-} from './types';
--- src/pages/ResumePage.tsx
+++ src/pages/ResumePage.tsx
@@ -1,127 +1,171 @@
 import React from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
 import * as data from '../data';
+import { Education, Job, Project, Skill, SkillCategory, SkillProficiency  } from '../data/types';
 import './ResumePage.css';
 
 export function ResumePage(): JSX.Element {
   return (
-    <>
-      <p><em><a href="/assets/documents/john_patterson_resume.pdf">Download a PDF copy by clicking here.</a></em></p>
-      <ResumeSection title="Work History">
-        {
-          data.jobs.map((entry, index) => (
-            <WorkEntry job={entry} key={index} />
-          ))
-        }
-      </ResumeSection>
-      <ResumeSection title="Education History">
-        {
-          data.education.map((entry, index) => (
-            <EducationEntry education={entry} key={index} />
-          ))
-        }
-      </ResumeSection>
-      <ResumeSection title="Projects">
-        {
-          data.projects.map((entry, index) => (
-            <ProjectEntry project={entry} key={index} />
-          ))
-        }
-      </ResumeSection>
-      <ResumeSection title="Skills">
-        <SkillsTable skills={data.skills} />
-      </ResumeSection>
-    </>
+    <Container fluid>
+      <Row>
+        <Col>
+          <em><a href="/assets/documents/john_patterson_resume.pdf">Download a PDF copy by clicking here.</a></em>
+        </Col>
+      </Row>
+      <Row>
+        <WorkHistorySection jobs={data.jobs} />
+      </Row>
+      <Row>
+        <EducationHistorySection education={data.education[0]} />
+      </Row>
+      <Row>
+        <ProjectsSection projects={data.projects} />
+      </Row>
+      <Row>
+        <SkillsSection skills={data.skills} />
+      </Row>
+    </Container>
   );
 }
 
-function SkillsTable(props: { skills: data.Skill[]}): JSX.Element {
-  const { skills } = props;
-  return (
-    <>
-    </>
-  );
-}
-
-function ProjectEntry(props: { project: data.Project }): JSX.Element {
-  const { project } = props;
-  return (
-    <div>
-      <div className="entry-row">{project.name}</div>
-    </div>
-  );
-}
-
-function WorkEntry(props: { job: data.Job }): JSX.Element {
-  const { job } = props;
-  return (
-    <div>
-      <div className="entry-row job-header">
-        <span>{job.company}</span>
-        <span>{job.from} – {job.to}</span>
-      </div>
-      <div className="entry-row job-title">
-        <span>{job.title}</span>
-        <span>{job.location}</span>
-      </div>
-      <div className="entry-row">
-        <ul>
-          {job.description.map((descr, index) => (
-            <li key={index}>
-              {descr}
-            </li>
-          ))}
-        </ul>
-      </div>
-    </div>
-  );
-}
-
-
-function EducationEntry(props: { education: data.Education }): JSX.Element {
-  const { education } = props;
-  return (
-    <div>
-      <div className="entry-row education-header">
-        <span>{education.university}</span>
-        <span>{education.from} – {education.to}</span>
-      </div>
-      <div className="entry-row job-title">
-        <span>
-          <div>{education.degrees[0]}</div>
-          <div>{education.degrees[1]}</div>
-        </span>
-        <span>{education.location}</span>
-      </div>
-      <div className="entry-row">
-        <ul>
-          {education.talkingPoints.map((point, index) => (
-            <li key={index}>
-              {point}
-            </li>
-          ))}
-        </ul>
-      </div>
-    </div>
-  );
-}
-
-
-interface ResumeSectionProps {
-  title: string;
-  children: React.ReactNode[] | React.ReactNode;
-}
-
-function ResumeSection(props: ResumeSectionProps): JSX.Element {
-  return (
-    <div className="resume-section">
-      <h1>{ props.title }</h1>
+function WorkHistorySection(props: { jobs: Job[] }): JSX.Element {
+  const jobs = props.jobs;
+    return (
+      <>
+      <h2>Work History</h2>
       {
-        React.Children.map(props.children, (child, index) => {
-          return (
-            <div className="resume-section-item" key={index}>{child}</div>
-          );
-        })
+        jobs.map((job, idx) => (
+          <Container fluid key={idx}>
+            <Row>
+              <Col>{job.company}</Col>
+              <Col className="text-right">{job.from} to {job.to}</Col>
+            </Row>
+            <Row>
+              <Col>{job.title}</Col>
+              <Col className="text-right">{job.location}</Col>
+            </Row>
+            <Row>
+              <ul>
+                {
+                  job.description.map((line, idx) => (
+                    <li key={`ul-${idx}`}>{line}</li>
+                  ))
+                }
+              </ul>
+            </Row>
+          </Container>
+        ))
       }
-    </div>
+      </>
+    );
+}
+
+function EducationHistorySection(props: { education: Education }): JSX.Element {
+  const education = props.education;
+  return (
+    <>
+    <h2>Education</h2>
+    <Container fluid>
+      <Row>
+        <Col>{education.university}</Col>
+        <Col className="text-right">{education.from} to {education.to}</Col>
+      </Row>
+      <Row>
+        <Col>{education.degrees[0]}</Col>
+        <Col className="text-right">{education.location}</Col>
+      </Row>
+      <Row>
+        <Col>{education.degrees[1]}</Col>
+      </Row>
+      <Row>
+        <ul>
+          {
+            education.talkingPoints.map((point, idx) => (
+              <li key={`ul-${idx}`}>{point}</li>
+            ))
+          }
+        </ul>
+      </Row>
+    </Container>
+    </>
+  );
+}
+
+function ProjectsSection(props: { projects: Project[] }): JSX.Element {
+  const projects = props.projects;
+  return (
+    <>
+    <h2>Projects</h2>
+    {
+      projects.map((project, idx) => (
+        <Container fluid key={idx}>
+          <Row>
+            <Col>{project.name}</Col>
+            <Col className="text-right">
+              { project.sourceLink && (<a href={project.sourceLink}>Source</a>) }
+              { project.productionLink && (<a href={project.productionLink}>Live Site</a>) }
+            </Col>
+          </Row>
+          <Row>
+            { project.technologies.map((tech, idx) => <Col key={`tech-${idx}`}>{tech}</Col>)}
+          </Row>
+          <Row>
+            <p>{ project.description }</p>
+          </Row>
+        </Container>
+      ))
+    }
+    </>
+  );
+}
+
+type SkillsByCategory = Record<string, Record<string, Skill[]>>;
+function GroupSkillsByCategory(skills: Skill[]): SkillsByCategory {
+  const categoryNames = {
+    [SkillCategory.FRAMEWORK]: "Framework",
+    [SkillCategory.LANGUAGE]: "Language",
+    [SkillCategory.TOOL]: "Tool"
+  };
+  const skillProficiencies = {
+    [SkillProficiency.VERY]: "Very",
+    [SkillProficiency.NOT_VERY]: "Not Very"
+  };
+  let byCategory: SkillsByCategory = {};
+  for (const skill of skills) {
+    const categoryName = categoryNames[skill.category];
+    const skillLevel = skillProficiencies[skill.proficiency];
+    if (!(categoryName in byCategory)) {
+      byCategory[categoryName] = {};
+    }
+    if (!(skillLevel in byCategory[categoryName])) {
+      byCategory[categoryName][skillLevel] = [];
+    }
+    byCategory[categoryName][skillLevel].push(skill);
+  }
+  return byCategory;
+}
+
+function SkillsSection(props: { skills: Skill[] }): JSX.Element {
+  const categories = GroupSkillsByCategory(props.skills);
+
+  return (
+    <>
+    <h2>Skills</h2>
+    {
+      Object.keys(categories).map((categoryName, idx) => (
+        <Container fluid key={idx}>
+          <Row><Col>{categoryName}s</Col></Row>
+          { Object.keys(categories[categoryName]).sort().reverse().map((adeptness, idx) => (
+            <Row key={`adeptness-${idx}`}>
+              <Col>{adeptness}</Col>
+              { categories[categoryName][adeptness].map((skill, idx) => (
+                <Col key={`skill-${idx}`}>{skill.name}</Col>
+              ))}
+            </Row>
+          ))}
+        </Container>
+      ))
+    }
+    </>
   );
 }
